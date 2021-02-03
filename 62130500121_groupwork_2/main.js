b685c1abae55cebcb6989ff41eac893921e65dfe
Jirasin46033 <jirasin.46033@mail.kmutt.ac.th>
2021-02-03 11:40:47 +0700

    [update] index.html, main.js, myprofile.html, myprofile.js and preview.jpg

--- 62130500121_groupwork_2/main.js
+++ 62130500121_groupwork_2/main.js
@@ -1,9 +1,39 @@
-const app ={
+// const app ={
+//     data(){
+//         return{
+//             msg: 'Hello Vue 3'
+//         }
+//     }
+// }
+
+// Vue.createApp(app).mount('#app')
+
+const app = {
     data(){
         return{
-            msg: 'Hello Vue 3'
+            msg:'Hello  Vue3',
+            show:true,
+            url: 'https://www.google.com/' 
         }
+    },
+    created(){
+        console.log('created');
+    },
+    updated(){
+        console.log('updated');
     }
-}
+};
 
-Vue.createApp(app).mount('#app')
+// const app ={
+//     data(){
+//         return{
+//             name: 'Jirasin',
+//             text1: 'Hello guys, Nice to meet you!!',
+//             photos: 9,
+//             friends: 407,
+//             imgupper: 'https://wallpaperaccess.com/full/1931304.jpg'
+//         }
+//     }
+// }
+
+var mountedApp = Vue.createApp(app).mount('#app');
---
+++ 62130500121_groupwork_2/myprofile.js
@@ -0,0 +1,12 @@
+const app ={
+    data(){
+        return{
+            name: 'Jirasin',
+            text1: 'Hello guys, Nice to meet you!!',
+            photos: 9,
+            friends: 407,
+            imgupper: 'https://wallpaperaccess.com/full/1931304.jpg'
+        }
+    }
+}
+var mountedApp = Vue.createApp(app).mount('#app');
